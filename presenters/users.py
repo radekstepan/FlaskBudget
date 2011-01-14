456b60f2d017daf46be341aba6d2db8463382eff
Radek <radek.stepan@gmail.com>
2011-01-14 22:26:29 +0000

    Users error checking
    git-svn-id: http://radek.unfuddle.com/svn/radek_budget@19 138173b9-53cd-42c6-b595-420ba7eea4ed

--- presenters/users.py
+++ presenters/users.py
@@ -14,16 +14,27 @@ users = Module(__name__)
 @users.route('/user/add/private', methods=['GET', 'POST'])
 @login_required
 def add_private():
+    error = None
     if request.method == 'POST':
-        # create new private user associated with the current user
-        u = User(request.form['name'], session.get('logged_in_user'), True)
-        db_session.add(u)
-        db_session.commit()
+        # blank name?
+        if request.form['name']:
+            # already exists?
+            if not User.query\
+                .filter(User.associated_with == session.get('logged_in_user'))\
+                .filter(User.name == request.form['name']).first():
+                # create new private user associated with the current user
+                u = User(request.form['name'], session.get('logged_in_user'), True)
+                db_session.add(u)
+                db_session.commit()
 
-        # give the user a default account so we can do loans
-        a = Account(u.id, "Default", 'private', 0)
-        db_session.add(a)
+                # give the user a default account so we can do loans
+                a = Account(u.id, "Default", 'default', 0)
+                db_session.add(a)
 
-        db_session.commit()
-        flash('Private user added')
-    return render_template('admin_add_private_user.html')
+                db_session.commit()
+                flash('Private user added')
+            else:
+                error = 'You already have a user under that name'
+        else:
+            error = 'You need to provide a name'
+    return render_template('admin_add_private_user.html', **locals())
